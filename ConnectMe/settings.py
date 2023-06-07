eb925c47be60120219380aa0efc0f594a5944ac0
Chaeyeon Seo <ssallyseo@gmail.com>
2023-06-07 14:13:29 +0900

    MySQL setting

--- ConnectMe/settings.py
+++ ConnectMe/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 from decouple import config
+import my_settings
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -72,12 +73,7 @@ WSGI_APPLICATION = 'ConnectMe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
