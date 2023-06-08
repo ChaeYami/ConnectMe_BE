b35379d7de85ab5700240c37aafabac533be1d11
sangmin <leejeayu05@gmail.com>
2023-06-08 21:49:24 +0900

    [Feat] : place 대댓글 기능 추가
    -views 생성 : 댓글, 대댓글, 좋아요 기능 추가
    -views 추가 : 상세보기 댓글 시리얼라이저 추가
    -views 수정 : 권한 수정 (is_admin -> is_staff), Response 컨벤션 맞춰 변경
    -models 수정 : category, like 테이블 추가, image -> PlaceImage 모델로 분리
    -serializers 수정 : 이미지 시리얼라이저, 대댓글 시리얼라이저 추가 및 필드 순서 변경
    -urls 제거 : 대댓글 urls 제거

--- place/models.py
+++ place/models.py
@@ -3,27 +3,35 @@ from user.models import User
 
 
 class Place(models.Model):
-    # verbose name지정
+    CHOICES = [
+        ('밥','밥'),
+        ('술','술'),
+        ('카페','카페'),
+    ]
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='place_user')
-    title = models.CharField(max_length=50)
-    # 장소에서 이미지 여러개
-    image = models.ImageField(blank=True, null=True)
-    content = models.TextField()
-    bookmark = models.ManyToManyField(User, related_name='place_bookmark')
-    # 좋아요 추가하기
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+        User, on_delete=models.CASCADE, related_name='place_user', verbose_name='작성자')
+    title = models.CharField(max_length=50, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+    category = models.CharField(max_length=20, verbose_name='카테고리', choices=CHOICES)
+    bookmark = models.ManyToManyField(
+        User, related_name='place_bookmark', verbose_name='북마크')
+    like = models.ManyToManyField(
+        User, related_name='place_like', verbose_name='좋아요')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성날짜')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정날짜')
 
+class PlaceImage(models.Model):
+    image = models.ImageField(blank=True, null=True, verbose_name='이미지', upload_to="%Y/%m/%d")
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='게시글', related_name='place_image_place')
 
 class PlaceComment(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='place_comment_user')
+        User, on_delete=models.CASCADE, related_name='place_comment_user', verbose_name='작성자')
     place = models.ForeignKey(
-        Place, on_delete=models.CASCADE, related_name='place_comment_place')
-    content = models.TextField()
-    # 대댓글 구현
+        Place, on_delete=models.CASCADE, related_name='place_comment_place', verbose_name='게시글')
+    content = models.TextField(verbose_name='내용')
+    # 대댓글
     main_comment = models.ForeignKey(
-        'self', on_delete=models.CASCADE, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+        'self', on_delete=models.CASCADE, null=True, verbose_name='상위댓글', related_name='reply')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성날짜')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정날짜')
--- place/views.py
+++ place/views.py
@@ -10,8 +10,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import get_object_or_404
 
-# postman 연결확인 ✔️
 
+# ================================ 장소 게시글 시작 ================================
+# postman 연결확인 ✔️
 
 class PlaceView(APIView):
 
@@ -25,16 +26,16 @@ class PlaceView(APIView):
 
     # 장소 추천 작성하기
     def post(self, request):  # ✔️
-        if request.user.is_admin:
+        if request.user.is_staff:
             serializer = PlaceCreateSerializer(
                 data=request.data, context={"request": request})
             if serializer.is_valid():
                 serializer.save(user=request.user)
-                return Response('작성 성공')
+                return Response(serializer.data, status.HTTP_200_OK)
             else:
-                return Response({"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
         else:
-            return Response('권한 없음')
+            return Response({'message':'권한이 없습니다.'}, status.HTTP_403_FORBIDDEN)
 
 
 class PlaceDetailView(APIView):
@@ -44,8 +45,12 @@ class PlaceDetailView(APIView):
     # 장소 추천 상세보기
     def get(self, request, place_id):  # ✔️
         place = get_object_or_404(Place, id=place_id)
-        serializer = PlaceSerializer(place)
-        return Response(serializer.data)
+        query = place.place_comment_place.filter(main_comment=None)
+        
+        place_serializer = PlaceSerializer(place).data
+        comment_serializer = PlaceCommentSerializer(query, many=True).data
+
+        return Response({'place':place_serializer, 'comment':comment_serializer})
 
     # 장소 추천 북마크
     def post(self, request, place_id):  # ✔️
@@ -54,83 +59,108 @@ class PlaceDetailView(APIView):
 
         if user in place.bookmark.all():
             place.bookmark.remove(user)
-            return Response('구취')
+            return Response({"message":"북마크 취소"}, status.HTTP_200_OK)
         else:
             place.bookmark.add(user)
-            return Response('북맠')
+            return Response({"message":"북마크"}, status.HTTP_200_OK)
 
     # 장소 추천 수정하기
     def patch(self, request, place_id):  # ✔️
         place = get_object_or_404(Place, id=place_id)
 
         # 일부만 수정가능하도록 수정해야함
-        if request.user.is_admin:
+        if request.user.is_staff:
             serializer = PlaceCreateSerializer(place, data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response('place detail patch')
+                return Response(serializer.data, status.HTTP_200_OK)
             else:
-                return Response({"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
         else:
-            return Response('권한 없음')
+            return Response({'message':'권한이 없습니다.'}, status.HTTP_403_FORBIDDEN)
 
     # 장소 추천 삭제하기
     def delete(self, request, place_id):  # ✔️
         login = request.user
         writer = get_object_or_404(Place, id=place_id)
 
-        if login.is_admin:
+        if login.is_staff:
             writer.delete()
-            return Response('place detail delete⭕')
+            return Response(status.HTTP_200_OK)
         else:
-            return Response('place detail delete❌')
+            return Response({'message':'권한이 없습니다.'}, status.HTTP_403_FORBIDDEN)
+
+
+# 장소 추천 좋아요
+class PlaceLikeView(APIView):
+    def post(self, request, place_id):
+        place = get_object_or_404(Place, id=place_id)
+        if request.user in place.like.all():
+            place.like.remove(request.user)
+            return Response({"message":"좋아요 취소"}, status.HTTP_200_OK)
+        else:
+            place.like.add(request.user)
+            return Response({"message":"좋아요"}, status.HTTP_200_OK)
+
+
+# ================================ 장소 게시글 종료 ================================
+
+# ================================ 장소 댓글 시작 ================================
 
 
 class PlaceCommentView(APIView):
-    # 댓글 가져오기 작성
+
+    permission_classes = [IsAuthenticated]
+
+    # (프론트) 해당 게시글 댓글 가져오기
+    def get(self, request, place_id):  # ✔️
+        place = get_object_or_404(Place, id=place_id)
+        
+        query = place.place_comment_place.filter(main_comment=None)
+        serializer = PlaceCommentSerializer(query, many=True)
+            
+        return Response(serializer.data)
+
     # 댓글 작성
-    def post(self, request, place_id):
+    def post(self, request, place_id):  # ✔️
         serializer = PlaceCreateCommentSerializer(data=request.data)
+        place = get_object_or_404(Place, id=place_id)
+
         if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response('댓글작성⭕')
+            serializer.save(user=request.user, place=place)
+            return Response(serializer.data, status.HTTP_200_OK)
         else:
-            return Response('댓글작성❌')
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 
 class PlaceCommentDetailView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
     # 댓글 수정
-    def put(self, request, place_id, place_comment_id):
-        return Response('댓글 수정')
+    def put(self, request, place_id, place_comment_id):  # ✔️
+        comment = get_object_or_404(PlaceComment, id=place_comment_id)
+        if request.user == comment.user:
+            serializer = PlaceCreateCommentSerializer(
+                comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'message':'권한이 없습니다.'}, status.HTTP_403_FORBIDDEN)
 
     # 댓글 삭제
-    def delete(self, request, place_id, place_comment_id):
+    def delete(self, request, place_id, place_comment_id):  # ✔️
         login = request.user
         writer = get_object_or_404(PlaceComment, id=place_comment_id)
         if login == writer.user:
             writer.delete()
-            return Response('댓글 삭제⭕')
+            return Response(status.HTTP_200_OK)
         else:
-            return Response('댓글 삭제❌')
+            return Response({'message':'권한이 없습니다.'}, status.HTTP_403_FORBIDDEN)
 
 
-class PlaceReplyView(APIView):
-    # 대댓글 작성
-    # 대댓글 가져오기
-    def post(self, request, place_id, place_comment_id):
-        serializer = PlaceCreateCommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response('댓글작성⭕')
-        else:
-            return Response('댓글작성❌')
+# ================================ 장소 댓글 종료 ================================
 
-
-class PlaceReplyDetailView(APIView):
-    # 대댓글 수정
-    def put(self, request, place_id, place_comment_id, place_reply_id):
-        return Response('대댓글 수정')
-
-    # 대댓글 삭제
-    def delete(self, request, place_id, place_comment_id, place_reply_id):
-        return Response('대댓글 삭제')
