69c6c612961af73a45142c41c486552bb9bf8ac3
newstyle1443 <newstyle1443@gmail.com>
2023-06-11 20:16:01 +0900

    [Feat] : 검색 기능 추가, 다중 이미지 업로드 추가, 북마크 한 모임 리스트 추가 -requirements.txt에 검색에 필요한 django-filter 추가 -settings.py에 django-filter 사용하기 위한 부분 추가 -models.py에 모임 글에 이미지 추가하기 위한 모델 작성 -serializer에 모임 이미지 업로드 기능 작성 -urls.py에 검색과 북마크 기능 작성 -views.py에 모임 이미지 업로드 기능, 검색 기능 작성

--- meeting/serializer.py
+++ meeting/serializer.py
@@ -4,12 +4,29 @@ from meeting.models import (
     Meeting,
     MeetingComment,
     MettingCommentReply,
+    MeetingImage,
     )
-# ================================ 댓글, 대댓글 Nested Serializer 시작 ================================
 
+# ================================ 모임 이미지 시작 ================================ 
+
+class MeetingImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = MeetingImage
+        fields = "__all__"
+
+# ================================ 모임 이미지 끝 ================================ 
+
+# ================================ 댓글, 대댓글 Nested Serializer 시작 ================================
 # 따로 위에 올려놓은 이유는 모임 글 상세에 Nested Serializer 사용하기 위함입니다.
 # 모임 대댓글 리스트
+
 class MeetingCommentReplyListSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    def get_user(self, obj):
+        return obj.user.nickname
+    
     class Meta:
         model = MettingCommentReply
         fields = ("user","content","updated_at",)
@@ -17,41 +34,56 @@ class MeetingCommentReplyListSerializer(serializers.ModelSerializer):
 # 모임 댓글 리스트
 class MeetingCommentListSerializer(serializers.ModelSerializer):
     reply = MeetingCommentReplyListSerializer(many=True) # 대댓글 Nested Serializer
+
+    user = serializers.SerializerMethodField()
+    def get_user(self, obj):
+        return obj.user.nickname
+    
     class Meta:
         model = MeetingComment
         fields = ("user","content","updated_at","reply",)
 
 # ================================ 댓글, 대댓글 Nested Serializer 끝 ================================
 
-
 # ================================ 모임 글 리스트, 작성, 상세, 수정 ================================
-
 # 모임 글 리스트
 class MeetingListSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField
+    user = serializers.SerializerMethodField()
+    created_at = serializers.DateTimeField(format="%Y/%m/%d")
     def get_user(self, obj):
         return obj.user.nickname
 
     class Meta:
         model = Meeting
-        fields = ("title","user","created_at",)
+        fields = "__all__"
+        # fields = ("id","title","user","created_at",)
 
 # 모임 글 작성, 수정
 class MeetingCreateSerializer(serializers.ModelSerializer):
+    meeting_image = MeetingImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Meeting
-        fields = ("title","content",)
+        fields = ("title","content","meeting_image",)
+
+    def create(self, validated_data):
+        instance = Meeting.objects.create(**validated_data)
+        image_set = self.context['request'].FILES
+        for image_data in image_set.getlist('image'):
+            MeetingImage.objects.create(meeting=instance, image=image_data)
+        return instance
 
 # 모임 글 상세
 class MeetingDetailSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField
+    user = serializers.SerializerMethodField()
     comment = MeetingCommentListSerializer(many=True) # 댓글 Nested Serializer
+    meeting_image = MeetingImageSerializer(many=True, read_only=True)
     def get_user(self, obj):
         return obj.user.nickname
     
     class Meta:
         model = Meeting
-        fields = ("title","content","user","comment",)
+        fields = ("title","content","user","comment","meeting_image",)
 
 # ================================ 모임 글 리스트, 작성, 상세, 수정 끝 ================================
 
