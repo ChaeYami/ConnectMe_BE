15c88dd29996c153ae97a2c593d441e11c9e27a4
Chaeyeon Seo <ssallyseo@gmail.com>
2023-06-16 21:37:22 +0900

    [Fix] : create_superuser 버그수정

--- user/models.py
+++ user/models.py
@@ -44,7 +44,6 @@ class UserManager(BaseUserManager):
         user = self.create_user(account = account, password=password, **extra_fields)
 
         user.is_admin = True
-        user.is_staff = True
         user.is_active = True
         user.save(using=self._db)
         Profile.objects.create(user = user)
