261472cd769f6e5d29892ce82924c31ba934ad26
newstyle1443 <newstyle1443@gmail.com>
2023-06-28 12:06:29 +0900

    [Feat] 모임 참가 기능과 모임 상세에 핫플레이스 가져오기 -models.py 참가하기, 지역, 모임일, 인원수, 모집상태, 핫플레이스url 추가 -serializer.py 모델에 추가한 필드를 추가함 -urls.py 지원하기, 지역검색 api 추가 -views.py 참가하기 추가 -주석 docstring으로 변경함.

--- meeting/models.py
+++ meeting/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 from user.models import User
 # Create your models here.
-# ================================ 모임 글 모델 시작 ================================ 
+
+'''모임 글 모델 시작'''
 class Meeting(models.Model):
     user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE, related_name="my_meeting")
     title = models.CharField(verbose_name="제목", max_length=50)
@@ -10,18 +11,24 @@ class Meeting(models.Model):
     created_at = models.DateTimeField(verbose_name="작성일" , auto_now_add=True,)
     updated_at = models.DateTimeField(verbose_name="수정일" , auto_now=True)
     bookmark = models.ManyToManyField(User, verbose_name="북마크", blank=True, related_name="bookmark_meeting")
-# ================================ 모임 글 모델 끝 ================================ 
+    join_meeting = models.ManyToManyField(User, verbose_name="참가하기", blank=True, related_name="join_meeting")
+    meeting_city = models.CharField(verbose_name="지역", max_length=10)
+    meeting_at = models.TextField(verbose_name="모임일")
+    num_person_meeting = models.TextField(verbose_name="인원수",)
+    meeting_status = models.CharField(verbose_name="모집상태", max_length=10, default="모집중")
+    hot_place_url = models.TextField(verbose_name="핫플레이스URL")
+'''모임 글 모델 끝'''
 
-# ================================ 모임 댓글 시작 ================================ 
+'''모임 댓글 시작'''
 class MeetingComment(models.Model):
     user = models.ForeignKey(User, verbose_name="댓글작성자", on_delete=models.CASCADE)
     meeting = models.ForeignKey(Meeting, verbose_name="미팅", on_delete=models.CASCADE, related_name="comment") #Nested Serializer 사용을 위해 related_name 지정
     content = models.TextField(verbose_name="댓글내용")
     created_at = models.DateTimeField(verbose_name="작성일" , auto_now_add=True,)
     updated_at = models.DateTimeField(verbose_name="수정일" , auto_now=True)
-# ================================ 모임 댓글 모델 끝 ================================ 
+'''모임 댓글 모델 끝'''
 
-# ================================ 모임 대댓글 모델 시작 ================================ 
+'''모임 대댓글 모델 시작'''
 class MeetingCommentReply(models.Model):
     user = models.ForeignKey(User, verbose_name="대댓글작성자", on_delete=models.CASCADE)
     meeting = models.ForeignKey(Meeting, verbose_name="미팅", on_delete=models.CASCADE)
@@ -29,10 +36,10 @@ class MeetingCommentReply(models.Model):
     content = models.TextField(verbose_name="대댓글내용")
     created_at = models.DateTimeField(verbose_name="작성일" , auto_now_add=True,)
     updated_at = models.DateTimeField(verbose_name="수정일" , auto_now=True)
-# ================================ 모임 대댓글 모델 끝 ================================ 
+'''모임 대댓글 모델 끝'''
 
-# ================================ 모임 다중 이미지 시작 ================================ 
+'''모임 다중 이미지 시작'''
 class MeetingImage(models.Model):
     meeting = models.ForeignKey(Meeting, verbose_name="미팅", on_delete=models.CASCADE, related_name="meeting_image")
     image = models.ImageField(blank=True, null=True, verbose_name='이미지', upload_to="meeting_%Y/%m/%d")
-# ================================ 모임 다중 이미지 끝 ================================ 
+'''모임 다중 이미지 끝'''
