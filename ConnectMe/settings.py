cf19b57e59f545697a88a4c1d474ef12280109a2
NullisnotFalse <126448023+NullisnotFalse@users.noreply.github.com>
2023-07-07 10:45:00 +0900

    [Fix] : django rest framework api 문서 비활성화

--- ConnectMe/settings.py
+++ ConnectMe/settings.py
@@ -47,7 +47,10 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-    'DEFAULT_SCHEMA_CLASS': None,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 MIDDLEWARE = [
