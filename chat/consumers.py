4e94f0d45d2b10655aa456ee7266e7c6a3d462de
louis N <louis_n@naver.com>
2023-07-07 14:08:46 +0900

    [Feat] : 채팅 ver.1 완성
    - websocket 연결할 때 jwt 토큰 인증 기능 추가
    - 채팅창 참가 권한 인증 기능 추가
    - 인증 실패시 알림창 띄우는 기능 추가
    - 채팅 참가 / 떠남 메시지 추가
    - ChatRoom 테이블에 중복된 room_name 인스턴스가 생기는 현상 수정

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,8 +1,15 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-import json
+from django.db.models import Q
+
 from .models import ChatMessage, ChatRoom
 from user.models import User
+from django.conf import settings
+
+import json
+import jwt
+from datetime import datetime
+from urllib.parse import parse_qs
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -20,9 +27,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         chat_room = await database_sync_to_async(
             ChatRoom.objects.filter(room_name=room_name).first
         )()
-        author = data["from"]
+        author_id = data["from"]
         author_user = await database_sync_to_async(
-            User.objects.filter(nickname=author).first
+            User.objects.filter(id=author_id).first
         )()
         message = await database_sync_to_async(ChatMessage.objects.create)(
             chat_room=chat_room, author=author_user, content=data["message"]
@@ -55,7 +62,79 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
+        # 파라미터에서 token 추출
+        query_string = self.scope["query_string"].decode()
+        query_params = parse_qs(query_string)
+        token = query_params.get("token", [None])[0]
+
+        # 토큰이 없을 경우 WebSocket 연결 끊기
+        if not token:
+            await self.send_alert("로그인 해주세요.")
+            await self.close()
+            return
+
+        # jwt 토큰 decoding
+        try:
+            decoded_token = jwt.decode(
+                token, key=settings.SECRET_KEY, algorithms=["HS256"], verify=True
+            )
+
+        # 토큰이 유효하지 않은 경우 WebSocket 연결 끊기
+        except jwt.InvalidTokenError:
+            await self.send_alert("로그인이 만료되었습니다.")
+            await self.close()
+            return
+
+        room_name = self.room_name
+        participants = room_name.split("n")
+        self.user_id = str(decoded_token["user_id"])
+        user_id = self.user_id
+
+        # 채팅방 참가자가 아닐 경우 WebSocket 연결 끊기
+        if user_id not in participants:
+            await self.send_alert("채팅 참가 권한이 없습니다.")
+            await self.close()
+            return
+
+        # 채팅방 참가자일 경우 참가 메시지 보내기
+        user = await database_sync_to_async(User.objects.filter(id=user_id).first)()
+        nickname = user.nickname
+
+        join_message = f"{nickname}님이 채팅방에 참가했습니다."
+
+        message = {
+            "author": nickname,
+            "content": join_message,
+            "timestamp": str(datetime.now()),
+        }
+        content = {
+            "command": "new_message",
+            "message": message,
+        }
+        await self.send_chat_message(content)
+        return
+
     async def disconnect(self, close_code):
+        # 채팅방 참가자일 경우 나갈 때 메시지 보내기
+        room_name = self.room_name
+        participants = room_name.split("n")
+        user_id = str(self.user_id)
+        if user_id in participants:
+            user = await database_sync_to_async(User.objects.filter(id=user_id).first)()
+            nickname = user.nickname
+
+            exit_message = f"{nickname}님이 채팅방을 나갔습니다."
+
+            message = {
+                "author": nickname,
+                "content": exit_message,
+                "timestamp": str(datetime.now()),
+            }
+            content = {
+                "command": "new_message",
+                "message": message,
+            }
+            await self.send_chat_message(content)
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
@@ -73,3 +152,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event["message"]
         await self.send(text_data=json.dumps(message))
+
+    async def send_alert(self, message):
+        await self.send(text_data=json.dumps({"command": "alert", "message": message}))
