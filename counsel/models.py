66457589a3ec50b7a4fbb7dee5a3d5d870d08e4b
louis N <louis_n@naver.com>
2023-07-08 17:31:11 +0900

    [Delete] : 상담 글 태그 기능 삭제 - 함수 호환성 문제

--- counsel/models.py
+++ counsel/models.py
@@ -1,38 +1,63 @@
 from django.db import models
 from user.models import User
-from taggit.managers import TaggableManager
-
 
 
 class Counsel(models.Model):
     user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE)
     title = models.CharField(max_length=50, verbose_name="제목")
     content = models.TextField(verbose_name="내용")
-    like = models.ManyToManyField(User, related_name='counsel_like', verbose_name="좋아요")
-    created_at = models.DateTimeField(verbose_name="작성일" , auto_now_add=True,)
-    updated_at = models.DateTimeField(verbose_name="수정일" , auto_now=True)
-    is_anonymous = models.BooleanField(verbose_name="익명",null=True, blank=True)
-    tags = TaggableManager(blank=True, verbose_name="장고태그")
+    like = models.ManyToManyField(User, related_name="counsel_like", verbose_name="좋아요")
+    created_at = models.DateTimeField(
+        verbose_name="작성일",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(verbose_name="수정일", auto_now=True)
+    is_anonymous = models.BooleanField(verbose_name="익명", null=True, blank=True)
 
-    
+
 class CounselComment(models.Model):
-    user = models.ForeignKey(User, verbose_name="댓글작성자", on_delete=models.CASCADE, related_name="counsel_comment_user")
-    counsel= models.ForeignKey(Counsel, on_delete=models.CASCADE, related_name="counsel_comment_counsel")
+    user = models.ForeignKey(
+        User,
+        verbose_name="댓글작성자",
+        on_delete=models.CASCADE,
+        related_name="counsel_comment_user",
+    )
+    counsel = models.ForeignKey(
+        Counsel, on_delete=models.CASCADE, related_name="counsel_comment_counsel"
+    )
     content = models.TextField(verbose_name="내용")
-    like = models.ManyToManyField(User, related_name='counsel_comment_like', verbose_name="좋아요")
-    created_at = models.DateTimeField(verbose_name="작성일" , auto_now_add=True,)
-    updated_at = models.DateTimeField(verbose_name="수정일" , auto_now=True)
-    is_anonymous = models.BooleanField(verbose_name="익명",null=True, blank=True)
-    
-    
-class CounselReply(models.Model):
-    user = models.ForeignKey(User, verbose_name="댓글작성자", on_delete=models.CASCADE, related_name="counsel_reply_user")
-    counsel= models.ForeignKey(Counsel, on_delete=models.CASCADE)
-    comment = models.ForeignKey(CounselComment, verbose_name="댓글", on_delete=models.CASCADE, related_name="reply")
-    content = models.TextField(verbose_name="내용")
-    like = models.ManyToManyField(User, related_name='counsel_reply_like', verbose_name="좋아요")
-    created_at = models.DateTimeField(verbose_name="작성일" , auto_now_add=True,)
-    updated_at = models.DateTimeField(verbose_name="수정일" , auto_now=True)
-    is_anonymous = models.BooleanField(verbose_name="익명",null=True, blank=True)
-    
+    like = models.ManyToManyField(
+        User, related_name="counsel_comment_like", verbose_name="좋아요"
+    )
+    created_at = models.DateTimeField(
+        verbose_name="작성일",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(verbose_name="수정일", auto_now=True)
+    is_anonymous = models.BooleanField(verbose_name="익명", null=True, blank=True)
 
+
+class CounselReply(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name="댓글작성자",
+        on_delete=models.CASCADE,
+        related_name="counsel_reply_user",
+    )
+    counsel = models.ForeignKey(Counsel, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        CounselComment,
+        verbose_name="댓글",
+        on_delete=models.CASCADE,
+        related_name="reply",
+    )
+    content = models.TextField(verbose_name="내용")
+    like = models.ManyToManyField(
+        User, related_name="counsel_reply_like", verbose_name="좋아요"
+    )
+    created_at = models.DateTimeField(
+        verbose_name="작성일",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(verbose_name="수정일", auto_now=True)
+    is_anonymous = models.BooleanField(verbose_name="익명", null=True, blank=True)
--- counsel/serializers.py
+++ counsel/serializers.py
@@ -1,22 +1,16 @@
 from rest_framework import serializers
-from .models import (
-    Counsel,
-    CounselComment,
-    CounselReply
-)
-from taggit_serializer.serializers import(
-    TaggitSerializer,
-    TagListSerializerField
-)
+from .models import Counsel, CounselComment, CounselReply
 import bleach
 
 """ 대댓글 """
 
-'''대댓글 작성'''
+"""대댓글 작성"""
+
+
 class CounselReplyCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CounselReply
-        fields = ("content","is_anonymous")
+        fields = ("content", "is_anonymous")
         extra_kwargs = {
             "content": {
                 "error_messages": {
@@ -25,24 +19,26 @@ class CounselReplyCreateSerializer(serializers.ModelSerializer):
                 }
             },
         }
-        
+
     def validate(self, attrs):
-        content = attrs.get('content')
+        content = attrs.get("content")
         # content 필드에서 HTML 태그 제거
         cleaned_content = bleach.clean(content, tags=[], strip=True)
 
-        attrs['content'] = cleaned_content
+        attrs["content"] = cleaned_content
 
         return attrs
 
-'''대댓글'''
+
+"""대댓글"""
+
+
 class CounselReplySerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     reply_like_count = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(format="%Y.%m.%d %H:%M")
-    updated_at = serializers.DateTimeField(
-        format="%y.%m.%d %H:%M", read_only=True
-    )
+    updated_at = serializers.DateTimeField(format="%y.%m.%d %H:%M", read_only=True)
+
     def get_reply_like_count(self, obj):
         return obj.like.count()
 
@@ -52,36 +48,43 @@ class CounselReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = CounselReply
         fields = "__all__"
-        
+
 
 """ 댓글 """
+
 
 class CounselCommentSerializer(serializers.ModelSerializer):
     reply = CounselReplySerializer(many=True)
     user = serializers.SerializerMethodField()
     comment_like_count = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(format="%Y.%m.%d %H:%M")
-    updated_at = serializers.DateTimeField(
-        format="%y.%m.%d %H:%M", read_only=True
-    )
+    updated_at = serializers.DateTimeField(format="%y.%m.%d %H:%M", read_only=True)
 
     def get_user(self, obj):
-        return {"account": obj.user.account, "pk": obj.user.pk, "nickname": obj.user.nickname}
-    
+        return {
+            "account": obj.user.account,
+            "pk": obj.user.pk,
+            "nickname": obj.user.nickname,
+        }
+
     def get_comment_like_count(self, obj):
         return obj.like.count()
 
     class Meta:
         model = CounselComment
         fields = "__all__"
-        
-    
 
-'''댓글 작성'''
+
+"""댓글 작성"""
+
+
 class CounselCommentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CounselComment
-        fields = ("content","is_anonymous",)
+        fields = (
+            "content",
+            "is_anonymous",
+        )
         extra_kwargs = {
             "content": {
                 "error_messages": {
@@ -92,45 +95,52 @@ class CounselCommentCreateSerializer(serializers.ModelSerializer):
         }
 
     def validate(self, attrs):
-        content = attrs.get('content')
+        content = attrs.get("content")
 
         # content 필드에서 HTML 태그 제거
         cleaned_content = bleach.clean(content, tags=[], strip=True)
 
-        attrs['content'] = cleaned_content
+        attrs["content"] = cleaned_content
 
         return attrs
-    
+
+
 """ 글 작성, 상세, 수정 """
 
-'''글 리스트'''
-class CounselListSerializer(TaggitSerializer, serializers.ModelSerializer):
-    tags = TagListSerializerField()
+"""글 리스트"""
+
+
+class CounselListSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     comment_count = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(format="%Y.%m.%d")
-    
+
     def get_user(self, obj):
-        return {"account": obj.user.account, "pk": obj.user.pk, "nickname": obj.user.nickname}
-    
+        return {
+            "account": obj.user.account,
+            "pk": obj.user.pk,
+            "nickname": obj.user.nickname,
+        }
+
     def get_comment_count(self, obj):
-        reply_count = CounselReply.objects.filter(comment__in=obj.counsel_comment_counsel.all()).count()
-        return obj.counsel_comment_counsel.count()+reply_count
+        reply_count = CounselReply.objects.filter(
+            comment__in=obj.counsel_comment_counsel.all()
+        ).count()
+        return obj.counsel_comment_counsel.count() + reply_count
 
     class Meta:
         model = Counsel
         fields = "__all__"
 
-        
 
-'''글 작성, 수정'''
-class CounselCreateSerializer(TaggitSerializer, serializers.ModelSerializer):
-    tags = TagListSerializerField(required=False)
+"""글 작성, 수정"""
 
+
+class CounselCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Counsel
-        exclude = ['user', 'like', 'created_at', 'updated_at']
-        extra_kwargs={
+        exclude = ["user", "like", "created_at", "updated_at"]
+        extra_kwargs = {
             "title": {
                 "error_messages": {
                     "blank": "제목을 입력해주세요",
@@ -142,62 +152,39 @@ class CounselCreateSerializer(TaggitSerializer, serializers.ModelSerializer):
                 },
             },
         }
+
     def validate(self, attrs):
-            title = attrs.get('title')
-            content = attrs.get('content')
+        title = attrs.get("title")
+        content = attrs.get("content")
 
-            # title 필드에서 HTML 태그 제거
-            cleaned_title = bleach.clean(title, tags=[], strip=True)
+        # title 필드에서 HTML 태그 제거
+        cleaned_title = bleach.clean(title, tags=[], strip=True)
 
-            # content 필드에서 HTML 태그 제거
-            cleaned_content = bleach.clean(content, tags=[], strip=True)
+        # content 필드에서 HTML 태그 제거
+        cleaned_content = bleach.clean(content, tags=[], strip=True)
 
-            attrs['title'] = cleaned_title
-            attrs['content'] = cleaned_content
+        attrs["title"] = cleaned_title
+        attrs["content"] = cleaned_content
 
-            return attrs
-    
-    def create(self, validated_data):
-        tags_data = validated_data.pop('tags', [])
-        
-        if isinstance(tags_data, list):
-            tags_data = ','.join(tags_data)
-        
-        tags_list = [tag.strip() for tag in tags_data.split(',')]
+        return attrs
 
-        counsel = Counsel.objects.create(**validated_data)
-        counsel.tags.set(tags_list)
 
-        return counsel
+"""글 상세"""
 
-    def update(self, instance, validated_data):
-        tags_data = validated_data.pop('tags', [])
-        
-        if isinstance(tags_data, list):
-            tags_data = ','.join(tags_data)
-        
-        tags_list = [tag.strip() for tag in tags_data.split(',')]
 
-        instance = super().update(instance, validated_data)
-
-        instance.tags.set(tags_list)
-
-        return instance
-
-'''글 상세'''
-class CounselDetailSerializer(TaggitSerializer, serializers.ModelSerializer):
-    tags = TagListSerializerField()
+class CounselDetailSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y.%m.%d %H:%M")
     updated_at = serializers.DateTimeField(format="%Y.%m.%d %H:%M")
     user = serializers.SerializerMethodField()
     counsel_comment_counsel = CounselCommentSerializer
+
     def get_user(self, obj):
-        return {"account": obj.user.account, "pk": obj.user.pk, "nickname": obj.user.nickname}
-    
+        return {
+            "account": obj.user.account,
+            "pk": obj.user.pk,
+            "nickname": obj.user.nickname,
+        }
+
     class Meta:
         model = Counsel
         fields = "__all__"
-
-
-
-
